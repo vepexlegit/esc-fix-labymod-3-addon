ad5dd9e90a435633c75e44dc0a56a6fc85c30aa7
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-22 00:46:11 +0400

    Update ESCFixAddon.java

--- src/main/java/de/vepexlegit/escfix/ESCFixAddon.java
+++ src/main/java/de/vepexlegit/escfix/ESCFixAddon.java
@@ -1,30 +1,13 @@
 package de.vepexlegit.escfix;
 
 import net.labymod.api.LabyModAddon;
-import net.labymod.api.events.MessageSendEvent;
 import net.labymod.settings.elements.SettingsElement;
-import net.labymod.utils.Consumer;
-import net.labymod.utils.ServerData;
 import java.util.List;
 
 public class ESCFixAddon extends LabyModAddon {
     @Override
     public void onEnable() {
         this.getApi().registerForgeListener(this);
-
-        this.getApi().getEventManager().registerOnJoin(new Consumer<ServerData>() {
-            @Override
-            public void accept(ServerData serverData) {
-                
-            }
-        });
-
-        this.getApi().getEventManager().register(new MessageSendEvent() {
-            @Override
-            public boolean onSend(final String message) {
-                return message.startsWith("-");
-            }
-        });
         this.getApi().registerModule(new ESCFixModule());
     }
 
